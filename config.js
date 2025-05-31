8c000808142a068555a48aab53b26efdfeadd732
stopadesatpet <petstopadesat@gmail.com>
2025-06-01 01:47:45 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,123 @@
+const CONFIG = {
+    maintenanceMode: false,
+    allowedIP: 'kokotdexxik',
+    scripts: [
+        {
+            "id": 1,
+            "title": "okokBanking",
+            "description": "https://okok.tebex.io/package/5126428",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/okokBanking/",
+            "category": ["ESX", "Rare"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/CLjEXaf.png"
+        },
+        {
+            "id": 2,
+            "title": "Thug PVP Hud V1",
+            "description": "",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/ThugPVPHUD",
+            "category": ["Rare", "HUD"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/rf57Hor.png"
+        },
+        {
+            "id": 3,
+            "title": "Akre HUD - Edited",
+            "description": "https://akre-develop.tebex.io/package/6689032",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/AkreHUD",
+            "category": ["ESX", "Rare", "HUD"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/qLEwdkQ.png"
+        },
+        {
+            "id": 4,
+            "title": "DRC Drugs",
+            "description": "https://drc.tebex.io/package/5217333",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/DRCDrugs",
+            "category": "ESX",
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/9kpeLOb.png"
+        },
+        {
+            "id": 5,
+            "title": "GFX Chat",
+            "description": "https://gfx.tebex.io/package/5350715",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/GFXChat",
+            "category": "ESX",
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/gX4VGso.png"
+        },
+        {
+            "id": 6,
+            "title": "VMS Driving School",
+            "description": "https://www.youtube.com/watch?v=bLtowyy4L6M&ab_channel=vames",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/VMSDriveSchool",
+            "category": "ESX",
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/D13YzFp.png"
+        },
+        {
+            "id": 7,
+            "title": "VMS Gym",
+            "description": "https://www.youtube.com/watch?v=tJDv0pFwfKw&t=1s&ab_channel=vames",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/VMSGym",
+            "category": "ESX",
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/NiZllxT.png"
+        },
+        {
+            "id": 7,
+            "title": "VMS HUD",
+            "description": "https://www.youtube.com/watch?v=g2ofK2LqenY&ab_channel=vames",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/VMSHUD",
+            "category": ["ESX", "HUD"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/0HDupHK.png"
+        },
+        {
+            "id": 9,
+            "title": "AK4Y Case Opening",
+            "description": "https://ak4y.tebex.io/package/5348887",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/AK4YCaseOpening",
+            "category": ["ESX", "Rare"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/T4EcdPP.png"
+        },
+        {
+            "id": 10,
+            "title": "UJ Vespucci",
+            "description": "https://unclejust.tebex.io/package/5216674",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/UJVespucci",
+            "category": ["Rare", "MLO"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/W8PWcNl.png"
+        },
+        {
+            "id": 11,
+            "title": "Scully Radio V2",
+            "description": "https://scullys-development.tebex.io/package/5053702",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/ScullyRadio",
+            "category": ["ESX"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/gb97OiO.png"
+        },
+        {
+            "id": 12,
+            "title": "Vegas HUD",
+            "description": "",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/VegasHUD",
+            "category": ["ESX", "HUD"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/oXrWp7b.png"
+        },
+        {
+            "id": 13,
+            "title": "RDRP EUP",
+            "description": "",
+            "url": "https://www.5mscripts.eu/download-fgjrd-3923f-feodf/RDRPEUP",
+            "category": ["EUP"],
+            "price": "ZDARMA",
+            "image": "https://i.imgur.com/gro1qBC.png"
+        }
+    ]
+};
